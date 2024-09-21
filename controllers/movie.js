758bbed1383e8d59d70a9070a84e7657b5dfdced
massimiliano <massimarcello@hotmail.it>
2024-09-21 14:17:42 +0200

    upload

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,3 +1,5 @@
+import findMovie from '../utils/findMovie.js';
+
 let movies = [
     { id: 1, name: 'The Dark Knight', year: '2008', genre: 'Action' },
     { id: 2, name: 'Pulp Fiction', year: '1994', genre: 'Crime' },
@@ -6,68 +8,67 @@ let movies = [
     { id: 5, name: 'Forrest Gump', year: '1994', genre: 'Drama' }
 ];
 
+// const findMovie=(movies,id)=>{
+//     return movies.find((movie) => movie.id === id);
+// }
+
 const controllers = {
     getMovies: (req, res) => {
         res.status(200).json(movies);
     },
     getMovie: (req, res) => {
         const { id } = req.params;
-
-        const isMovie = movies.find((movie) => movie.id === id);
+        const isMovie = findMovie(movies, id);
         if (isMovie) {
-            res.status(200).json( {message: 'Movie:', isMovie });
+            res.status(200).json({ message: 'Movie:', isMovie });
         } else {
             res.status(404).json({ message: 'Movie not found' });
         }
     },
     addMovie: (req, res) => {
-        const{id,name,year,genre}=req.body
-if(!name || !year || !genre){
-         return   res.status(400).send('add a name,a year and a genre')
-         }
-         const newMovie={
-            id:movies.length + 1,
+        const { id, name, year, genre } = req.body;
+        if (!name || !year || !genre) {
+            return res.status(400).send('add a name,a year and a genre');
+        }
+        const newMovie = {
+            id: movies.length + 1,
             name,
             year,
             genre
-         }
-          movies.push(newMovie)
-          res.status(201).json(newMovie)
-          console.log(movies);
+        };
+        movies.push(newMovie);
+        res.status(201).json(newMovie);
+        console.log(movies);
     },
     updateMovie: (req, res) => {
-        const{name,year,genre}=req.body
-        const{id}=req.params
-let moviebyId =movies.find(movie=>movie.id===parseInt(id))
-if(!moviebyId){
-    return res.status(404).json({ error: 'Movie not found' });
-   
-  
-}else{
-    moviebyId.name=name 
-    moviebyId.year=year
-    moviebyId.genre=genre
- 
-    console.log(movies);
-    return  res.status(200).json({ message: 'Movie deleted', moviebyId})
-}
-  
+        const { name, year, genre } = req.body;
+        const { id } = req.params;
+        let moviebyId = findMovie(movies, id);
+        if (!moviebyId) {
+            return res.status(404).json({ error: 'Movie not found' });
+        } else {
+            moviebyId.name = name;
+            moviebyId.year = year;
+            moviebyId.genre = genre;
 
-
-        
+            console.log(movies);
+            return res
+                .status(200)
+                .json({ message: 'Movie updated', moviebyId });
+        }
     },
     deleteMovie: (req, res) => {
         const { id } = req.params;
-        const movie = movies.find(movie => movie.id === parseInt(id));
-        
+        const movie = findMovie(movies, id);
+
         if (!movie) {
             return res.status(404).json({ error: 'Movie not found' });
         }
-    
-        movies = movies.filter(movie => movie.id !== parseInt(id));
+
+        movies = movies.filter((movie) => movie.id !== parseInt(id));
         console.log(movies);
-        res.status(200).json({ message: 'Movie deleted', movies });
-    }}
-    
+        res.status(200).json({ movie_deleted: 'Movie deleted', movie, new_list:'New movie List',movies });
+    }
+};
 
 export default controllers;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+// import movie routes
 import movieRoutes from './routes/movie.js';
+// import middleware
+import createLog from './middleware/createLog.js';
+import { path, PATH } from './utils/fileSystem.js';
 
 // configure dotenv
 dotenv.config();
@@ -9,24 +13,25 @@ const PORT = process.env.PORT || 5009;
 // initialize express
 const app = express();
 
-// parse body 
+// parse body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware di logging
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-});
+// Middleware logging
+app.use(createLog);
+// serve static file
+app.use(express.static(path.join(PATH, 'public')));
+
+// Set the view engine to ejs
+app.set('views', path.join(PATH, 'views'));
+app.set('view engine', 'ejs');
 
 // use routes
-app.use('/api', movieRoutes); 
-
-
+app.use('/api', movieRoutes);
 
 // handle 404
 app.use('*', (req, res) => {
-    res.status(404).json({ message: 'Page is not found' });
+    res.status(404).render('404');
 });
 // error handling
 app.use((err, req, res, next) => {
